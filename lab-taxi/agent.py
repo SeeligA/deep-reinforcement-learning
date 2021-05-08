0c7471957972c807035fc76a20e25504702642d3
SeeligA <as@arndtseelig.com>
2021-05-08 20:35:58 +0200

    Implement Expected Sarsa with deterministic drop-off

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -15,7 +15,7 @@ class Agent:
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
         self.epsilon = 1.0
-        self.alpha = 1.0
+        self.alpha = 0.2
         self.gamma = 1.0
         self.episode = 1
 
@@ -30,11 +30,33 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
+
+        def decode(i):
+            out = []
+            out.append(i % 4)
+            i = i // 4
+            out.append(i % 5)
+            i = i // 5
+            out.append(i % 5)
+            i = i // 5
+            out.append(i)
+            assert 0 <= i < 5
+            return out[::-1]
+
+        locs = [(0, 0), (0, 4), (4, 0), (4, 3)]
+        s = decode(state)
+        taxi_loc = tuple(s[:2])
+        pass_idx = s[2]
+        dest_idx = s[3]
+
+        if pass_idx == 4:
+            if taxi_loc == locs[dest_idx]:
+                 return 5
+
         if random.random() > self.epsilon:
             return np.argmax(self.Q[state])
 
-        return np.random.choice(self.nA)
-
+        return np.random.choice(4)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -53,7 +75,9 @@ class Agent:
         if done:
             self.Q[state][action] += self.alpha * reward - self.Q[state][action]
         else:
-            self.Q[state][action] += self.alpha * (
-                reward + self.gamma * self.Q[next_state][np.argmax(self.Q[next_state])] - self.Q[state][action])
+            probs = np.ones(self.nA) * self.epsilon / self.nA
+            probs[np.argmax(self.Q[next_state])] += 1 - self.epsilon
+            self.Q[state][action] += self.alpha * (reward + self.gamma * np.dot(probs, self.Q[next_state]) - self.Q[state][action])
+
 
         self.episode += 1

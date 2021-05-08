708c2d4c2891db5ae53afaf4b268dbca1adb143d
SeeligA <as@arndtseelig.com>
2021-05-08 18:20:54 +0200

    Implement Q-Learning as Taxi baseline

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from collections import defaultdict
 
@@ -12,6 +14,10 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 1.0
+        self.alpha = 1.0
+        self.gamma = 1.0
+        self.episode = 1
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +30,11 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
+        if random.random() > self.epsilon:
+            return np.argmax(self.Q[state])
+
         return np.random.choice(self.nA)
+
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +47,13 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+
+        self.epsilon /= self.episode
+
+        if done:
+            self.Q[state][action] += self.alpha * reward - self.Q[state][action]
+        else:
+            self.Q[state][action] += self.alpha * (
+                reward + self.gamma * self.Q[next_state][np.argmax(self.Q[next_state])] - self.Q[state][action])
+
+        self.episode += 1
--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -3,6 +3,10 @@ from monitor import interact
 import gym
 import numpy as np
 
-env = gym.make('Taxi-v2')
-agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+def main():
+    env = gym.make('Taxi-v3')
+    agent = Agent()
+    avg_rewards, best_avg_reward = interact(env, agent)
+
+if __name__ == "__main__":
+    main()
